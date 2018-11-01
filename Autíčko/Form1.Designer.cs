2c9e06ad15523ceb525ea589175ed80b7bfbb4d8
dev <tomas.pospisil180695@gmail.com>
2018-11-01 21:17:45 +0100

    Primocary kruhovy pohyb

---
+++ Autíčko/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Autíčko
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tlZrychli = new System.Windows.Forms.Button();
+            this.tlZpomal = new System.Windows.Forms.Button();
+            this.časovač = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tlZrychli
+            // 
+            this.tlZrychli.Location = new System.Drawing.Point(119, 162);
+            this.tlZrychli.Name = "tlZrychli";
+            this.tlZrychli.Size = new System.Drawing.Size(75, 23);
+            this.tlZrychli.TabIndex = 0;
+            this.tlZrychli.Text = "Zrychli";
+            this.tlZrychli.UseVisualStyleBackColor = true;
+            this.tlZrychli.Click += new System.EventHandler(this.tlZrychli_Click);
+            // 
+            // tlZpomal
+            // 
+            this.tlZpomal.Location = new System.Drawing.Point(285, 162);
+            this.tlZpomal.Name = "tlZpomal";
+            this.tlZpomal.Size = new System.Drawing.Size(75, 23);
+            this.tlZpomal.TabIndex = 1;
+            this.tlZpomal.Text = "Zpomal";
+            this.tlZpomal.UseVisualStyleBackColor = true;
+            this.tlZpomal.Click += new System.EventHandler(this.tlZpomal_Click);
+            // 
+            // časovač
+            // 
+            this.časovač.Enabled = true;
+            this.časovač.Interval = 50;
+            this.časovač.Tick += new System.EventHandler(this.časovač_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 217);
+            this.Controls.Add(this.tlZpomal);
+            this.Controls.Add(this.tlZrychli);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tlZrychli;
+        private System.Windows.Forms.Button tlZpomal;
+        private System.Windows.Forms.Timer časovač;
+    }
+}
+
---
+++ Autíčko/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Autíčko
+{
+    public partial class Form1 : Form
+    {
+        int šířkaOkna;
+
+        //Kinematické parametry auta
+
+        double xStart = 10;
+        double x;
+        int y0 = 10;
+        int rychlost = 0;
+
+        //Geometrické parametry auta
+        int šířkaAuta = 110;
+        int výškaAuta = 30;
+        int poziceKola1 = 30;
+        int poziceKola2 = 80;
+        int poloměrKola = 20;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics kp = e.Graphics;
+
+            //Výchozí bod autíčka je [x0, y0]
+            int x0 = Convert.ToInt32(x);
+
+            //Karoserie
+            kp.FillRectangle(Brushes.Pink, x0, y0, šířkaAuta, výškaAuta);
+
+            //Silnice
+            int ySilnice = y0 + výškaAuta + poloměrKola;
+            kp.DrawLine(Pens.LightGray, 0, ySilnice, šířkaOkna - 1, ySilnice);
+
+            //Výpočet natočení kol
+            double ujeto = x - xStart;
+            double obvodKola = 2 * Math.PI * poloměrKola;
+            double otáčekKola = ujeto / obvodKola;
+            double desetinnáČást = otáčekKola - Math.Truncate(otáčekKola);
+            double úhelNatočení = 2 * Math.PI * desetinnáČást;
+
+                //Již v radiánech
+
+            //1.kolo
+
+            int xStředu = x0 + poziceKola1;
+            int yStředu = y0 + výškaAuta;
+            int xLH = xStředu - poloměrKola;
+            int yLH = yStředu - poloměrKola;
+            kp.DrawEllipse(Pens.Black, xLH, yLH, 2 * poloměrKola, 2 * poloměrKola);
+
+            //Čárka na 1. kole
+            double rozdílX = poloměrKola * Math.Cos(úhelNatočení);
+            double rozdílY = poloměrKola * Math.Sin(úhelNatočení);
+            int xKonce = Convert.ToInt32(xStředu + rozdílX);
+            int yKonce = Convert.ToInt32(yStředu + rozdílY);
+            kp.DrawLine(Pens.Black, xStředu, yStředu, xKonce, yKonce);
+
+            //posun na druhé kolo
+            int posun = poziceKola2 - poziceKola1;
+            xStředu += posun;
+            xLH += posun;
+            xKonce += posun;
+
+            //2 kolo a čárka na něm
+
+            kp.DrawEllipse(Pens.Black, xLH, yLH, 2 * poloměrKola, 2 * poloměrKola);
+            kp.DrawLine(Pens.Black, xStředu, yStředu, xKonce, yKonce);
+
+
+
+
+
+        }
+
+        private void časovač_Tick(object sender, EventArgs e)
+        {
+            //Převod jednotek ( z milisekund na sekundy)
+            double čas = 0.001 * časovač.Interval;
+
+            //Posun autíčka
+            x += rychlost * čas;
+
+            //pokud za okrajem, přesun na začátek
+            if (x >= šířkaOkna)
+            {
+                double posunZpět = šířkaOkna + šířkaAuta;
+                x -= posunZpět;
+                xStart -= posunZpět;
+
+
+
+            }
+            //překresli
+            Refresh();
+        }
+
+        private void tlZrychli_Click(object sender, EventArgs e)
+        {
+            rychlost += 10;
+        }
+
+        private void tlZpomal_Click(object sender, EventArgs e)
+        {
+            rychlost -= 10;
+            if (rychlost < 0)
+            {
+                rychlost = 0;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            x = xStart;
+            šířkaOkna = ClientSize.Width;
+        }
+    }
+}
